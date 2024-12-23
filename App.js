66b747d510374360fff7f516b171b340cc19bbc7
seanharrys <heyembedme@gmail.com>
2024-12-23 06:41:56 -1000

    App.js

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { NoteProvider } from './src/context/NoteContext';
 import HomeScreen from './src/screens/HomeScreen';
 import NoteScreen from './src/screens/NoteScreen';
@@ -13,14 +12,31 @@ export default function App() {
   return (
     <NoteProvider>
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator 
+          initialRouteName="Home"
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: '#f8f8f8',
+            },
+            headerTintColor: '#000',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        >
           <Stack.Screen 
             name="Home" 
             component={HomeScreen} 
             options={{ title: 'My Notes' }}
           />
           <Stack.Screen name="Note" component={NoteScreen} />
-          <Stack.Screen name="EditNote" component={EditNoteScreen} />
+          <Stack.Screen 
+            name="EditNote" 
+            component={EditNoteScreen}
+            options={({ route }) => ({
+              title: route.params?.noteId ? 'Edit Note' : 'New Note',
+            })}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </NoteProvider>
